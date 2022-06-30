0a853553853752f78cc94d8723b4d3bc8cde490b
dipam2612 <dipam2612@gmail.com>
2022-06-30 14:35:23 +0530

    my commit

---
+++ src/test/java/files/MyntraOperations.java
@@ -0,0 +1,16 @@
+package files;
+
+import org.openqa.selenium.WebDriver;
+
+import utilities.SeleniumUtility;
+
+public class MyntraOperations extends SeleniumUtility {
+
+	//WebDriver driver;
+	SeleniumUtility su=new SeleniumUtility();
+	WebDriver driver=su.setUp("chrome", "https://www.myntra.com/");
+	
+	//div[@class='desktop-navContent']/div/a[@data-group='men']
+	//driver.
+	
+}
